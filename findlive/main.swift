098ddcf7f1b112fd0a010bb83dca27ac851f63c9
Martin Gabriel <martin.gabriel91@gmail.com>
2019-08-03 00:47:59 +0200

    Initial Commit

---
+++ findlive/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  findlive
+//
+//  Created by Martin Gabriel on 03/08/2019.
+//  Copyright © 2019 Martin Gabriel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
