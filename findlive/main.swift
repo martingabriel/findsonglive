88fdcac34b10b48c6a1c5479410ee88aba005841
Martin Gabriel <martin.gabriel91@gmail.com>
2019-08-03 02:43:03 +0200

    Add readme & example

--- findlive/main.swift
+++ findlive/main.swift
@@ -2,11 +2,39 @@
 //  main.swift
 //  findlive
 //
+//  little utility to open browser with youtube search of currently playing song in iTunes
+//
 //  Created by Martin Gabriel on 03/08/2019.
 //  Copyright © 2019 Martin Gabriel. All rights reserved.
 //
 
 import Foundation
+import Cocoa
+import ScriptingBridge
 
-print("Hello, World!")
+@objc protocol iTunesApplication {
+    @objc optional func currentTrack()-> AnyObject
+    @objc optional var properties: NSDictionary {get}
+}
 
+let iTunesApp: AnyObject = SBApplication(bundleIdentifier: "com.apple.iTunes")!
+let trackDict = iTunesApp.currentTrack!().properties as Dictionary
+// if nil then no current track
+if (trackDict["name"] != nil) {
+    let songName = trackDict["name"] as! String
+    let artist = trackDict["artist"] as! String
+    
+    var searchUrl = "https://www.youtube.com/results?search_query="
+    searchUrl.append(artist.replacingOccurrences(of: " ", with: "+"))
+    searchUrl.append("+")
+    searchUrl.append(songName.replacingOccurrences(of: " ", with: "+"))
+    searchUrl.append("+live")
+    
+    if let url = URL(string: searchUrl) {
+        // open youtube search in default browser
+        if NSWorkspace.shared.open(url) {
+            print("Youtube search with song " + songName + " was opened.")
+            
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# findsonglive
+
+Little tool for youtube search of live version of song that you currently listening in iTunes. Tested on macOS 10.14 with SWIFT5.
+
+## Easy to use
+
+Run findlive from terminal and it will open your default browser page with youtube search of currently playing song.
+
+## Example
+
+![example](example.gif)
+
